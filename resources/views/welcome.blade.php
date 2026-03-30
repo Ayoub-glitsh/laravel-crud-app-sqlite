1adde9adb6cbedf870b968e30ceb4e217f527eb0
Ayoub-glitsh <ayoubaguezzar1@gmail.com>
2026-03-30 19:06:44 +0100

    Update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,10 @@
 <x-layout>
-    <h1>Welcome to Ninja Network</h1>
-    <p>Click the button below to view the list of ninjas</p>
-    <a href="{{ route('ninjas.index') }}" class="btn">Find Ninjas!</a>
+    <div class="text-center px-8 py-12">
+  <h1>Welcome to the Ninja Network</h1>
+  <p>Click the button below to view the list of ninjas.</p>
+  
+  <a href="/ninjas" class="btn mt-4 inline-block">
+    Find Ninjas!
+  </a>
+</div>
 </x-layout>
